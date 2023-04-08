b51cec0ab5bebdba2a58d07077aeb592e90ebe3f
Hazzardgg <kirya_nazarov_2002@mail.ru>
2023-04-09 00:50:44 +0300

    v1.1

--- Task05/Task05_1/tests/BookListTest.php
+++ Task05/Task05_1/tests/BookListTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
 class BookListTest extends TestCase
 {
     static $bl1;
-    public static function setUpBeforeClass() : void
+    public static function setUpBeforeClass(): void
     {
         $b1 = new Book();
         $b1->setTitle("Капитанская дочка")
@@ -38,55 +38,81 @@ class BookListTest extends TestCase
         self::$bl1->next();
         self::$bl1->next();
         self::$bl1->rewind();
-        $this->assertSame(self::$bl1->get(0),
-        self::$bl1->current());
+        $this->assertSame(
+            self::$bl1->get(0),
+            self::$bl1->current()
+        );
     }
     public function testNext()
     {
         self::$bl1->next();
-        $this->assertSame(self::$bl1->get(1),
-        self::$bl1->current());
+        $this->assertSame(
+            self::$bl1->get(1),
+            self::$bl1->current()
+        );
         self::$bl1->next();
-        $this->assertSame(self::$bl1->get(2),
-        self::$bl1->current());
+        $this->assertSame(
+            self::$bl1->get(2),
+            self::$bl1->current()
+        );
     }
     public function testValid()
     {
         self::$bl1->rewind();
-        $this->assertSame(true,
-        self::$bl1->valid());
+        $this->assertSame(
+            true,
+            self::$bl1->valid()
+        );
         self::$bl1->next();
-        $this->assertSame(true,
-        self::$bl1->valid());
+        $this->assertSame(
+            true,
+            self::$bl1->valid()
+        );
         self::$bl1->next();
-        $this->assertSame(true,
-        self::$bl1->valid());
+        $this->assertSame(
+            true,
+            self::$bl1->valid()
+        );
         self::$bl1->next();
-        $this->assertSame(false,
-        self::$bl1->valid());
+        $this->assertSame(
+            false,
+            self::$bl1->valid()
+        );
     }
     public function testValue()
     {
         self::$bl1->rewind();
-        $this->assertSame(self::$bl1->get(0),
-        self::$bl1->current());
+        $this->assertSame(
+            self::$bl1->get(0),
+            self::$bl1->current()
+        );
         self::$bl1->next();
-        $this->assertSame(self::$bl1->get(1),
-        self::$bl1->current());
+        $this->assertSame(
+            self::$bl1->get(1),
+            self::$bl1->current()
+        );
         self::$bl1->next();
-        $this->assertSame(self::$bl1->get(2),
-        self::$bl1->current());
+        $this->assertSame(
+            self::$bl1->get(2),
+            self::$bl1->current()
+        );
     }
     public function testKey()
     {
         self::$bl1->rewind();
-        $this->assertSame(self::$bl1->get(0)->getId(),
-        self::$bl1->key());
+        $this->assertSame(
+            self::$bl1->get(0)->getId(),
+            self::$bl1->key()
+        );
         self::$bl1->next();
-        $this->assertSame(self::$bl1->get(1)->getId(),
-        self::$bl1->key());
+        $this->assertSame(
+            self::$bl1->get(1)->getId(),
+            self::$bl1->key()
+        );
         self::$bl1->next();
-        $this->assertSame(self::$bl1->get(2)->getId(),
-        self::$bl1->key());
+        $this->assertSame(
+            self::$bl1->get(2)->getId(),
+            self::$bl1->key()
+        );
     }
 }
--- Task05/Task05_2/tests/TruncaterTest.php
+++ Task05/Task05_2/tests/TruncaterTest.php
@@ -8,27 +8,43 @@ use PHPUnit\Framework\TestCase;
 class TruncaterTest extends TestCase
 {
     static $t1;
-    public static function setUpBeforeClass() : void
+    public static function setUpBeforeClass(): void
     {
         self::$t1 = new Truncater();
     }
     public function testTruncating()
     {
         $test_string = 'Назаров Кирилл Викторович';
-        $this->assertSame('',
-        self::$t1->truncate(''));
-        $this->assertSame($test_string,
-        self::$t1->truncate($test_string));
-        $this->assertSame('Назаров Ки...',
-        self::$t1->truncate($test_string, ['length' => 10]));
-        $this->assertSame($test_string,
-        self::$t1->truncate($test_string, ['length' => 50]));
-        $this->assertSame('Назаров Кирилл ...',
-        self::$t1->truncate($test_string, ['length' => -10]));
-        $this->assertSame('Назаров Ки*',
-        self::$t1->truncate($test_string,
-        ['length' => 10, 'separator' => '*']));
-        $this->assertSame($test_string,
-        self::$t1->truncate($test_string, ['separator' => '*']));
+        $this->assertSame(
+            '',
+            self::$t1->truncate('')
+        );
+        $this->assertSame(
+            $test_string,
+            self::$t1->truncate($test_string)
+        );
+        $this->assertSame(
+            'Назаров Ки...',
+            self::$t1->truncate($test_string, ['length' => 10])
+        );
+        $this->assertSame(
+            $test_string,
+            self::$t1->truncate($test_string, ['length' => 50])
+        );
+        $this->assertSame(
+            'Назаров Кирилл ...',
+            self::$t1->truncate($test_string, ['length' => -10])
+        );
+        $this->assertSame(
+            'Назаров Ки*',
+            self::$t1->truncate(
+                $test_string,
+                ['length' => 10, 'separator' => '*']
+            )
+        );
+        $this->assertSame(
+            $test_string,
+            self::$t1->truncate($test_string, ['separator' => '*'])
+        );
     }
 }
